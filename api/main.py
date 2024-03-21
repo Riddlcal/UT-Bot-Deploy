50e33b88918909ab86f74a6b1ed6423a472f3c10
Riddlcal <127689960+Riddlcal@users.noreply.github.com>
2024-03-21 13:12:50 -0500

    Update main.py

--- api/main.py
+++ api/main.py
@@ -56,7 +56,7 @@ if index_name not in pc.list_indexes().names():
 retriever = BM25Retriever.from_documents(chunked_documents, k=2)
 
 # Initialize Chat models
-llm_name = 'gpt-3.5-turbo'
+llm_name = 'gpt-4-1106-preview'
 qa = ConversationalRetrievalChain.from_llm(
     ChatOpenAI(openai_api_key=openai_api_key, model=llm_name),
     retriever,
