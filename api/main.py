d4033c7d398da60fd9808229b8b83abf3e887577
Riddlcal <127689960+Riddlcal@users.noreply.github.com>
2024-03-27 16:55:43 -0500

    Update main.py

--- api/main.py
+++ api/main.py
@@ -1,13 +1,12 @@
-import itertools
-import asyncio
-import random
 from flask import Flask, render_template, request
 from langchain_community.document_loaders import TextLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain_openai import OpenAIEmbeddings, ChatOpenAI
-from pinecone import Pinecone, PodSpec
-from langchain.chains import ConversationalRetrievalChain, RetrievalQA
-from langchain_pinecone import PineconeVectorStore
+from langchain_openai import OpenAIEmbeddings
+from pinecone import Pinecone
+from langchain_openai import ChatOpenAI
+from langchain.chains import ConversationalRetrievalChain
+from langchain_community.retrievers import BM25Retriever
+from langchain.prompts.prompt import PromptTemplate
 from bs4 import BeautifulSoup
 import time
 import os
@@ -31,18 +30,19 @@ chunked_documents = []
 for doc in documents:
     chunked_documents.extend(text_splitter.split_documents([doc]))
 
-# Initialize APIs
-pinecone_api_key = os.getenv('PINECONE_API_KEY')
+# Initialize OpenAI Embeddings
 openai_api_key = os.getenv('OPENAI_API_KEY')
+model_name = 'text-embedding-3-small'
+embeddings = OpenAIEmbeddings(openai_api_key=openai_api_key, model_name=model_name)
 
-# Use pod-based architecture
-spec = PodSpec(environment='GCP')
+# Initialize Pinecone
+pinecone_api_key = os.getenv('PINECONE_API_KEY')
+
+# Now do stuff with Pinecone
+index_name = "chatdata"
 
 # Initialize Pinecone with the provided information
-pc = Pinecone(api_key=pinecone_api_key)
-
-# Set index name
-index_name = "chatdata"
+pc = Pinecone(api_key=pinecone_api_key, cloud="GCP", environment="gcp-starter", region="us-central1")
 
 # Check if the index already exists; if not, create it
 if index_name not in pc.list_indexes().names():
@@ -50,46 +50,17 @@ if index_name not in pc.list_indexes().names():
         name=index_name,
         dimension=1536,  # Length of OpenAI embeddings
         metric='cosine',  # or any other metric you prefer
-        spec=spec  # specify the correct variant and environment
+        spec={"pod": "starter"}  # specify the correct variant and environment
     )
 
-# Wait for index to be initialized
-while not pc.describe_index(index_name).status['ready']:
-    time.sleep(1)
-
-# Initialize LangChain embeddings object
-model_name = 'text-embedding-3-small'
-embeddings = OpenAIEmbeddings(model=model_name, openai_api_key=openai_api_key)
-
-def generate_example_data(vector_dim, vector_count):
-    for i in range(vector_count):
-        yield f'id-{i}', [random.random() for _ in range(vector_dim)]
-
-# Define the vector dimension and count
-vector_dim = 1536
-vector_count = 10000
-
-# Example generator that generates many (id, vector) pairs
-example_data_generator = generate_example_data(vector_dim, vector_count)
-
-# Define the chunks function to break an iterable into chunks
-def chunks(iterable, batch_size=10):
-    it = iter(iterable)
-    chunk = tuple(itertools.islice(it, batch_size))
-    while chunk:
-        yield chunk
-        chunk = tuple(itertools.islice(it, batch_size))
-
-async def upsert_data_async(pc, index_name, example_data_generator, batch_size):
-    index = pc.Index(index_name)
-    for ids_vectors_chunk in chunks(example_data_generator, batch_size):
-        await index.upsert(vectors=ids_vectors_chunk, async_req=True)
+# Create a BM25Retriever instance with documents
+retriever = BM25Retriever.from_documents(chunked_documents, k=2)
 
 # Initialize Chat models
 llm_name = 'gpt-3.5-turbo'
 qa = ConversationalRetrievalChain.from_llm(
     ChatOpenAI(openai_api_key=openai_api_key, model=llm_name),
-    None,  # No need for retriever
+    retriever,
     return_source_documents=True
 )
 
@@ -101,14 +72,6 @@ If you don't know the answer, say simply that you cannot help with the question 
 
 {question}
 """
-
-# Initialize the LangChain vector store
-text_field = "text"
-vectorstore = PineconeVectorStore(index_name, embeddings, text_field)
-
-# Initialize RetrievalQA object
-llm = ChatOpenAI(openai_api_key=openai_api_key, model_name=llm_name, temperature=0.0)
-qa = RetrievalQA.from_chain_type(llm=llm, chain_type="stuff", retriever=vectorstore.as_retriever())
 
 # Define route for home page
 @app.route('/')
@@ -127,14 +90,59 @@ def ask():
 
     # Remove labels like '[Label]' from the answer
     answer = re.sub(r'\[[^\[\]]+\]', '', answer)
-
+    
     # Check if the answer contains iframe HTML
     if 'iframe' in answer:
         return render_template('iframe.html', iframe_html=answer)
     else:
         # Remove unnecessary characters such as parentheses
         cleaned_answer = re.sub(r'[()\[\]]', '', answer)
-
+    
         # Initialize Beautiful Soup
         soup = BeautifulSoup(cleaned_answer, 'html.parser')
+    
+        # Find all URLs and email addresses in the text
+        urls = re.findall(r'\bhttps?://\S+\b', str(soup))
+        emails = re.findall(r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b', str(soup))
+        
+        # Replace each URL with an anchor tag
+        for url in urls:
+            # Create a new anchor tag
+            new_tag = soup.new_tag('a', href=url, target='_blank', rel='noopener noreferrer')
+            # Append icon to the link
+            icon_tag = BeautifulSoup('<i class="fa-solid fa-arrow-up-right-from-square" style="margin-left: 10px;"></i>', 'html.parser')
+            new_tag.append('Click here ')
+            new_tag.append(icon_tag)
+            # Replace the URL with the anchor tag
+            soup = BeautifulSoup(str(soup).replace(url, str(new_tag)), 'html.parser')
+        
+        # Replace each email address with a mailto link
+        for email in emails:
+            # Create a new anchor tag
+            new_tag = soup.new_tag('a', href='mailto:' + email)
+            new_tag.append('Contact ')
+            # Append icon to the link
+            icon_tag = BeautifulSoup('<i class="fa-solid fa-envelope" style="margin-left: 10px;"></i>', 'html.parser')
+            new_tag.append(icon_tag)
+            # Replace the email with the anchor tag
+            email_tag_str = str(new_tag)
+            soup = BeautifulSoup(str(soup).replace(email, email_tag_str), 'html.parser')
+        
+        # Convert back to string and remove any loose characters after links
+        answer_with_links = str(soup).strip().rstrip('/. ')
+        
+        # Add line breaks
+        answer_with_line_breaks = answer_with_links.replace('\n', '<br>')
+        
+        # Check if bullet points are needed
+        if '•' in answer_with_line_breaks:
+            # Split the answer into lines and wrap each line with <li> tags
+            lines = answer_with_line_breaks.split('\n')
+            bulleted_lines = '<ul>' + ''.join([f'<li>{line}</li>' for line in lines]) + '</ul>'
+            return bulleted_lines
+        else:
+            return answer_with_line_breaks
 
+# Run the Flask app
+if __name__ == '__main__':
+    app.run(debug=True)
