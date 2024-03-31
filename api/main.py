6c480f41014a64cd7f306edb44cd43d3703ac2af
Riddlcal <127689960+Riddlcal@users.noreply.github.com>
2024-03-31 14:48:18 -0500

    Update main.py

--- api/main.py
+++ api/main.py
@@ -1,5 +1,3 @@
-import os
-import json
 from flask import Flask, render_template, request
 from langchain_community.document_loaders import TextLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
@@ -11,6 +9,7 @@ from langchain_community.retrievers import BM25Retriever
 from langchain.prompts.prompt import PromptTemplate
 from bs4 import BeautifulSoup
 import time
+import os
 import warnings
 import re
 
@@ -25,23 +24,24 @@ file_path = "UT Bot.txt"
 loader = TextLoader(file_path)
 documents = loader.load()
 
-# Initialize text splitter
-text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
-
 # Split documents into chunks
-chunked_documents = text_splitter.split_documents(documents)
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+chunked_documents = []
+for doc in documents:
+    chunked_documents.extend(text_splitter.split_documents([doc]))
 
 # Initialize OpenAI Embeddings
 openai_api_key = os.getenv('OPENAI_API_KEY')
 model_name = 'text-embedding-3-small'
-embed_model = OpenAIEmbeddings(openai_api_key=openai_api_key, model_name=model_name)
-
-# Generate embeddings for the documents
-document_embeddings = embed_model.embed_documents(chunked_documents)
+embeddings = OpenAIEmbeddings(openai_api_key=openai_api_key, model_name=model_name)
 
 # Initialize Pinecone
 pinecone_api_key = os.getenv('PINECONE_API_KEY')
+
+# Now do stuff with Pinecone
 index_name = "chatdata"
+
+# Initialize Pinecone with the provided information
 pc = Pinecone(api_key=pinecone_api_key, cloud="GCP", environment="gcp-starter", region="us-central1")
 
 # Check if the index already exists; if not, create it
@@ -53,15 +53,14 @@ if index_name not in pc.list_indexes().names():
         spec={"pod": "starter"}  # specify the correct variant and environment
     )
 
-# Store embeddings in Pinecone
-for idx, doc_embedding in enumerate(document_embeddings):
-    pc.upsert(index_name=index_name, vectors=[doc_embedding], ids=[str(idx)])
+# Create a BM25Retriever instance with documents
+retriever = BM25Retriever.from_documents(chunked_documents, k=2)
 
 # Initialize Chat models
 llm_name = 'gpt-3.5-turbo'
 qa = ConversationalRetrievalChain.from_llm(
     ChatOpenAI(openai_api_key=openai_api_key, model=llm_name),
-    BM25Retriever.from_pinecone(pc, index_name),
+    retriever,
     return_source_documents=True
 )
 
@@ -91,21 +90,21 @@ def ask():
 
     # Remove labels like '[Label]' from the answer
     answer = re.sub(r'\[[^\[\]]+\]', '', answer)
-
+    
     # Check if the answer contains iframe HTML
     if 'iframe' in answer:
         return render_template('iframe.html', iframe_html=answer)
     else:
         # Remove unnecessary characters such as parentheses
         cleaned_answer = re.sub(r'[()\[\]]', '', answer)
-
+    
         # Initialize Beautiful Soup
         soup = BeautifulSoup(cleaned_answer, 'html.parser')
-
+    
         # Find all URLs and email addresses in the text
         urls = re.findall(r'\bhttps?://\S+\b', str(soup))
         emails = re.findall(r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b', str(soup))
-
+        
         # Replace each URL with an anchor tag
         for url in urls:
             # Create a new anchor tag
@@ -116,7 +115,7 @@ def ask():
             new_tag.append(icon_tag)
             # Replace the URL with the anchor tag
             soup = BeautifulSoup(str(soup).replace(url, str(new_tag)), 'html.parser')
-
+        
         # Replace each email address with a mailto link
         for email in emails:
             # Create a new anchor tag
@@ -128,13 +127,13 @@ def ask():
             # Replace the email with the anchor tag
             email_tag_str = str(new_tag)
             soup = BeautifulSoup(str(soup).replace(email, email_tag_str), 'html.parser')
-
+        
         # Convert back to string and remove any loose characters after links
         answer_with_links = str(soup).strip().rstrip('/. ')
-
+        
         # Add line breaks
         answer_with_line_breaks = answer_with_links.replace('\n', '<br>')
-
+        
         # Check if bullet points are needed
         if '•' in answer_with_line_breaks:
             # Split the answer into lines and wrap each line with <li> tags
