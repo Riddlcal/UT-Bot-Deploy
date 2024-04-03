be455c5adb425608db7a7106c5ba2a70098ef5b1
Caleb Riddlesperger <127689960+Riddlcal@users.noreply.github.com>
2024-04-03 13:45:31 -0500

    Update cron.js

--- api/cron.js
+++ api/cron.js
@@ -1,7 +1,7 @@
 const { exec } = require('child_process');
 
 // Define the command to execute
-const command = 'python -m pip install faiss-cpu';
+const command = 'sudo pip install faiss-cpu';
 
 // Execute the command
 exec(command, (error, stdout, stderr) => {
