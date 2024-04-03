cd2c5e748b6aba25c2011d5e6cabb2d500b2a15d
Caleb Riddlesperger <127689960+Riddlcal@users.noreply.github.com>
2024-04-03 14:40:47 -0500

    Update cron.js

--- api/cron.js
+++ api/cron.js
@@ -1,21 +1,5 @@
 const { exec } = require('child_process');
 
-// Define the command to check pip version
-const versionCommand = 'pip --version';
-
-// Execute the version command
-exec(versionCommand, (versionError, versionStdout, versionStderr) => {
-  if (versionError) {
-    console.error(`Error checking pip version: ${versionError.message}`);
-    return;
-  }
-  if (versionStderr) {
-    console.error(`stderr: ${versionStderr}`);
-    return;
-  }
-
-  console.log(`Pip version: ${versionStdout}`);
-
   // Define the command to install faiss-cpu
   const installCommand = 'pip install faiss-cpu';
 
